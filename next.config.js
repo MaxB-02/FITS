0adeac2b262eb3d8cba67cde5a2d805e88d85fb0
Max Brenner <mlb59723@creighton.edu>
2025-08-13 20:17:53 -0500

    Fix next.config.js to use CommonJS format for AWS Amplify compatibility

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-      },
-      {
-        protocol: 'https',
-        hostname: 'via.placeholder.com',
-      },
-    ],
-  },
-}
+};
 
-export default nextConfig 
+module.exports = nextConfig; 
